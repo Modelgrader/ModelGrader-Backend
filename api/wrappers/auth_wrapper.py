41151cb8f53f28a877700bd29409ffb65242fe14
kanon.che <kanon.che@ascendcorp.com>
2025-09-11 16:42:33 +0700

    Implement wrapper for auth and permission

--- api/wrappers/auth_wrapper.py
+++ api/wrappers/auth_wrapper.py
@@ -1,3 +1,4 @@
+from api.services.auth_service import verifyToken
 from api.utility import extract_bearer_token
 from api.errors.common import InvalidTokenError
 
@@ -6,6 +7,8 @@ def validate_token(function):
         token = extract_bearer_token(request)
         if not token:
             raise InvalidTokenError()
+        is_verify = verifyToken(token)
+        if not is_verify:
+            raise InvalidTokenError()
         return function(request, token=token, *args, **kwargs)
     return wrapper
-
