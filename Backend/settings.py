f02c247ec13a04d037019ce789a4cc2bbf1fc4e1
KanonKC <kanonkce@gmail.com>
2025-09-12 23:20:25 +0700

    Create request extension

--- Backend/settings.py
+++ Backend/settings.py
@@ -70,12 +70,13 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+    'api.middleware.CustomRequestMiddleware',  # Custom middleware to replace HttpRequest with CustomRequest
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'Backend.urls'
